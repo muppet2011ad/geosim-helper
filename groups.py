34e26c2dac9fce3115f3cd060b823c9fe3b906bb
James Townsend <j.a.townsend2000@gmail.com>
2020-04-29 12:26:48 +0100

    Myriad bugfixes
    1) No longer ruined by trailing spaces on /wiki/organisations
    2) 2ics are now successfully identified as claimants

--- groups.py
+++ groups.py
@@ -1,9 +1,9 @@
 import praw
 
 class Org(object):
-    def __init__(self, name, claims):
-        self.name = name
-        self.claims = claims
+	def __init__(self, name, claims):
+		self.name = name
+		self.claims = claims
 
 
 def getOrgs():
@@ -13,15 +13,15 @@ def getOrgs():
 	orgswiki = geosim.wiki["organisations"].content_md
 	lines = orgswiki.split("\n")
 	claimsingroup = []
-	groupname = lines[0][2:-3]
+	groupname = lines[0].replace("*", "").replace("\r", "").rstrip()
 	for line in lines[1:-1]:
-		if line == "\n":
+		if line.replace(" ", "").replace("\r", "") == "":
 			continue
 		if line[0] == "*":
 			orgs.append(Org(groupname, claimsingroup))
 			claimsingroup = []
-			groupname = line[2:-3]
+			groupname = line.replace("*", "").replace("\r", "").rstrip()
 		elif line[0:-1] != "":
-			claimsingroup.append(line[0:-1])
+			claimsingroup.append(line.replace("\r", "").rstrip())
 	orgs.append(Org(groupname, claimsingroup))
 	return orgs
